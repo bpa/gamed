e26391ddf6c42d35e50178946e9d788b333ff486
Bruce Armstrong <bruce@fortressofgeekdom.org>
2006-03-31 00:33:57 +0000

    A simple py2exe script for making bundles

---
+++ clients/py-SpeedRisk/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import py2exe, glob
+
+setup(windows=["SpeedRisk.py"],
+        data_files=[
+            ("images",glob.glob("images\\*.png")),
+            ("images",glob.glob("images\\*.jpg")),
+            ("images",["images\\manifest"]),
+        ],
+        includes=['Numeric'],
+     )
+
