7375cf1774e11e3e0f05292b0c9457b8b10590c8
Bruce Armstrong <bruce@fortressofgeekdom.org>
2008-07-11 07:22:03 +0000

    Forgot to add this when checking in the last update

---
+++ server/command.h
@@ -0,0 +1,25 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+/* I'm cheating because I use a standard convention of
+ * cmd and client
+ */
+#define PLAYER_ERROR(s) \
+	cmd->command = CMD_ERROR; \
+	cmd->subcmd = s; \
+	cmd->length = 0; \
+	server_funcs.tell_player(&client->player, &buff[0], 4);
+
+#define CMD_PLAYER(c, s) \
+	cmd->command = c; \
+	cmd->subcmd = s; \
+	cmd->length = 0; \
+	server_funcs.tell_player(&client->player, &buff[0], 4);
+
+#define CMD_ALL_LEN(c, s) \
+	cmd->command = c; \
+	cmd->subcmd = s; \
+	cmd->length = htons(buff_len); \
+	server_funcs.tell_all(client->game, &buff[0], buff_len+4);
+
+#endif
